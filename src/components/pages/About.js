cad73b817423215671024cd43c6e87e094856af5
inuzen <condddr@gmail.com>
2019-09-27 08:15:35 +0300

    hello world, kinda

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+
+
+const About = (props) => (
+  <Fragment>
+    <h1>About this app</h1>
+    <p>App to search github</p>
+    <p>Version 1.0.0</p>
+  </Fragment>
+);
+
+About.propTypes = {
+
+};
+
+export default About;
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () =>{
+  return (
+    <div><h1>Not Found</h1><p className="lead">The page you're looking for doesn't exist</p></div>
+  )
+}
+
+
+export default Home;
